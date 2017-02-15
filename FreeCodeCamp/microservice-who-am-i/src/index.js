15bb93bba7205f09c60daa9172591a4076f58200
C <dreifarbenralle@gmail.com>
2017-02-14 21:21:39 -0500

    added d3 bar chart

---
+++ FreeCodeCamp/microservice-who-am-i/src/index.js
@@ -0,0 +1,14 @@
+// var express = require("express");
+var microservice = require("./microservice.js");
+document.write(microservice);
+
+// var app = express();
+// var port = process.env.PORT || 3000;
+//
+// app.get("/", function(request, response){
+//   response.send("hey there");
+// });
+//
+// app.listen(port, function(){
+//   console.log( "I'm listenin':" + port)
+// })
